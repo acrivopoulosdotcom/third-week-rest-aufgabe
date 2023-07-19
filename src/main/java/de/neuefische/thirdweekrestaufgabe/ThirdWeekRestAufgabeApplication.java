a0c84e6d80151c4eebc2d3c420ad83a5174be79b
clenn <acrivopoulosdotcom@gmail.com>
2023-07-19 08:08:58 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/thirdweekrestaufgabe/ThirdWeekRestAufgabeApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.thirdweekrestaufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdWeekRestAufgabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdWeekRestAufgabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/thirdweekrestaufgabe/ThirdWeekRestAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.thirdweekrestaufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdWeekRestAufgabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
